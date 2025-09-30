56f1437cb1ff293dacba017235d7b477c016f5eb
hwankim123 <hwankim1941@gmail.com>
2025-09-30 20:40:50 +0900

    gitignore

---
+++ src/main/java/com/hwan/lazyant/LazyantApplication.java
@@ -0,0 +1,13 @@
+package com.hwan.lazyant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyantApplication.class, args);
+	}
+
+}
