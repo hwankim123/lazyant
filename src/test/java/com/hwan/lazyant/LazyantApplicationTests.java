c22a1acfcbd9db03f7a6b182bab7cca705779cbc
hwankim123 <hwankim1941@gmail.com>
2025-09-30 20:24:17 +0900

    init

---
+++ src/test/java/com/hwan/lazyant/LazyantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwan.lazyant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
