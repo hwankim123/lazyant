d0f1b15d851e9e1ea9d4326ea7aa4afc9a6a2adb
hwankim123 <hwankim1941@gmail.com>
2025-10-04 17:19:18 +0900

    Fix: tradingType enum을 ordinal로 저장하도록 수정

--- src/main/resources/db/migration/2510_1__add_tradings.sql
+++ src/main/resources/db/migration/2510_1__add_tradings.sql
@@ -2,9 +2,9 @@ CREATE TABLE la_tradings (
 	id bigserial primary key,
 	stock_id bigserial not null,
 	account_id bigserial not null,
-	trading_type varchar(64) not null,
+	trading_type smallint not null,
 	current_market_price float8 not null,
 	volume float8 not null,
-	memo varchar(256),
+	memo varchar(256) null,
 	trade_time timestamp not null
 );
